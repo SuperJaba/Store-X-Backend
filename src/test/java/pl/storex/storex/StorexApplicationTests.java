0e1f3e00ed955db61dc6cd017d38c03f9dbe2ac6
Sebastian Piechowski <duneval@gmail.com>
2024-03-05 00:22:35 +0100

    initial commit for project settings

---
+++ src/test/java/pl/storex/storex/StorexApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.storex.storex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
